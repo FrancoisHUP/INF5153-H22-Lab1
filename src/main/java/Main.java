0c4ea96342a7ba5a327e06b8a75d3c4791f6e4f9
francoisHUP <>
2022-01-20 01:28:22 -0500

    version initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Money money = new Money(10,"CAN");
+
+        System.out.println(money);
+    }
+}
+
+
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,2 +1,19 @@
 public class Money {
+
+    private int amount;
+    private String currency;
+
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
